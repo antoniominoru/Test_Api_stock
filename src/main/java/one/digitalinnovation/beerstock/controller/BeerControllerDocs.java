1fa466faf795f74bf28f535882d4391a8e58efee
antoniominoru <antonio.minoru.ops@gmail.com>
2022-03-17 12:45:57 -0300

    update

--- src/main/java/one/digitalinnovation/beerstock/controller/BeerControllerDocs.java
+++ src/main/java/one/digitalinnovation/beerstock/controller/BeerControllerDocs.java
@@ -2,12 +2,20 @@ package one.digitalinnovation.beerstock.controller;
 
 
 import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
 import one.digitalinnovation.beerstock.exception.BeerNotFoundException;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 public interface BeerControllerDocs {
+
+    BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException;
 
     BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException;
 
+    List<BeerDTO> listBeers();
+
+    void deleteById(@PathVariable Long id) throws BeerNotFoundException;
 
 }
--- src/main/java/one/digitalinnovation/beerstock/repository/BeerRepository.java
+++ src/main/java/one/digitalinnovation/beerstock/repository/BeerRepository.java
@@ -3,6 +3,8 @@ package one.digitalinnovation.beerstock.repository;
 import one.digitalinnovation.beerstock.entity.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BeerRepository extends JpaRepository<Beer, Long> {
-    Beer findByName(String name);
+    Optional<Beer> findByName(String name);
 }
